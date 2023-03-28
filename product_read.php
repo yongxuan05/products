fa557e6e90d814542978d59ab386395bf8ae3bf9
Lai Yong Xuan <yongxuan213012@dasein.edu.my>
2023-03-28 11:32:20 +0800

    create product_read & Preduct_read_one

---
+++ product_read.php
@@ -0,0 +1,101 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Read</title>
+    <meta charset="utf-8">
+    <!-- Latest compiled and minified Bootstrap CSS (Apply your Bootstrap here -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@600&display=swap" rel="stylesheet">
+
+
+</head>
+
+<body>
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Read Products</h1>
+        </div>
+
+        <!-- PHP code to read records will be here -->
+
+        <?php
+        // include database connection
+        include 'config/database.php';
+
+        // delete message prompt will be here
+
+        // select all data
+        $query = "SELECT * FROM products";
+        $stmt = $con->prepare($query);
+        $stmt->execute();
+
+        // this is how to get number of rows returned
+        $num = $stmt->rowCount();
+
+        // link to create record form
+        echo "<a href='product_create.php' class='btn btn-primary m-b-1em'>Create New Product</a>";
+
+        //check if more than 0 record found
+        if ($num > 0) {
+
+            // data from database will be here
+            echo "<table class='table table-hover table-responsive table-bordered'>"; //start table
+
+            //creating our table heading
+            echo "<tr>";
+            echo "<th>ID</th>";
+            echo "<th>Name</th>";
+            echo "<th>Description</th>";
+            echo "<th>Price</th>";
+            echo "<th>Action</th>";
+            echo "</tr>";
+
+            // retrieve our table contents
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                // extract row
+                // this will make $row['firstname'] to just $firstname only
+                extract($row);
+
+                // creating new table row per record
+                echo "<tr>";
+                echo "<td>{$id}</td>";
+                echo "<td>{$name}</td>";
+                echo "<td>{$description}</td>";
+                echo "<td>{$price}</td>";
+                echo "<td>";
+
+                // read one record
+                echo "<a href='product_read_one.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
+
+                // we will use this links on next part of this post
+                echo "<a href='update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
+
+                // we will use this links on next part of this post
+                echo "<a href='#' onclick='delete_user({$id});'  class='btn btn-danger'>Delete</a>";
+                echo "</td>";
+                echo "</tr>";
+            }
+
+
+            // end table
+            echo "</table>";
+        }
+        // if no records found
+        else {
+            echo "<div class='alert alert-danger'>No records found.</div>";
+        }
+        ?>
+
+
+
+    </div> <!-- end .container -->
+
+    <!-- confirm delete record will be here -->
+
+</body>
+
+</html>
---
+++ product_read_one.php
@@ -0,0 +1,91 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Read Products</title>
+    <meta charset="utf-8">
+    <!-- Latest compiled and minified Bootstrap CSS (Apply your Bootstrap here -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@600&display=swap" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Read Product</h1>
+        </div>
+
+        <!-- PHP read one record will be here -->
+        <?php
+        // get passed parameter value, in this case, the record ID
+        // isset() is a PHP function used to verify if a value is there or not
+        $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+
+        //include database connection
+        include 'config/database.php';
+
+        // read current record's data
+        try {
+            // prepare select query
+            $query = "SELECT id, name, description, price FROM products WHERE id = ? LIMIT 0,1";
+            $stmt = $con->prepare($query);
+
+            // this is the first question mark
+            $stmt->bindParam(1, $id);
+
+            // execute our query
+            $stmt->execute();
+
+            // store retrieved row to a variable
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // values to fill up our form
+            $name = $row['name'];
+            $description = $row['description'];
+            $price = $row['price'];
+        }
+
+        // show error
+        catch (PDOException $exception) {
+            die('ERROR: ' . $exception->getMessage());
+        }
+        ?>
+
+
+
+        <!-- HTML read one record table will be here -->
+        <!--we have our html table here where the record will be displayed-->
+        <table class='table table-hover table-responsive table-bordered'>
+            <tr>
+                <td>Name</td>
+                <td><?php echo htmlspecialchars($name, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Description</td>
+                <td><?php echo htmlspecialchars($description, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Price</td>
+                <td><?php echo htmlspecialchars($price, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <a href='index.php' class='btn btn-danger'>Back to read products</a>
+                </td>
+            </tr>
+        </table>
+
+
+
+    </div> <!-- end .container -->
+
+</body>
+
+</html>
