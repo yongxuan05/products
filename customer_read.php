5caeba327aa0ee0333191c40dbd1e3c915587c0a
Lai Yong Xuan <yongxuan213012@dasein.edu.my>
2023-03-29 19:49:43 +0800

    create customer read

---
+++ customer_read.php
@@ -0,0 +1,144 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Read Customer</title>
+    <meta charset="utf-8">
+    <!-- Latest compiled and minified Bootstrap CSS (Apply your Bootstrap here -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@600&display=swap" rel="stylesheet">
+
+
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="navigation bar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">navbar<span>.</span></a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="product_create.php">Create Product</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="product_read.php">Read Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="customer_create.php">Create Customer</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="customer_read.php">Read Customer</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.html">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- end Navbar -->
+
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Read Customers</h1>
+        </div>
+
+        <!-- PHP code to read records will be here -->
+
+        <?php
+        // include database connection
+        include 'config/database.php';
+
+        // delete message prompt will be here
+
+        // select all data
+        $query = "SELECT * FROM customers";
+        $stmt = $con->prepare($query);
+        $stmt->execute();
+
+        // this is how to get number of rows returned
+        $num = $stmt->rowCount();
+
+        // link to create record form
+        echo "<a href='customer_create.php' class='btn btn-primary m-b-1em'>Create New Customer</a>";
+
+        //check if more than 0 record found
+        if ($num > 0) {
+
+            // data from database will be here
+            echo "<table class='table table-hover table-responsive table-bordered'>"; //start table
+
+            //creating our table heading
+            echo "<tr>";
+            echo "<th>ID</th>";
+            echo "<th>Username</th>";
+            echo "<th>First Name</th>";
+            echo "<th>Last Name</th>";
+            echo "<th>Gender</th>";
+            echo "<th>Date of Birth</th>";
+            echo "<th>Register Date & Time</th>";
+            echo "<th>Status</th>";
+            echo "<th>Action</th>";
+            echo "</tr>";
+
+            // retrieve our table contents
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                // extract row
+                // this will make $row['firstname'] to just $firstname only
+                extract($row);
+
+                // creating new table row per record
+                echo "<tr>";
+                echo "<td>{$id}</td>";
+                echo "<td>{$username}</td>";
+                echo "<td>{$fname}</td>";
+                echo "<td>{$lname}</td>";
+                echo "<td>{$gender}</td>";
+                echo "<td>{$dob}</td>";
+                echo "<td>{$register}</td>";
+                echo "<td>{$status}</td>";
+                echo "<td>";
+
+                // read one record
+                echo "<a href='product_read_one.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
+
+                // we will use this links on next part of this post
+                echo "<a href='update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
+
+                // we will use this links on next part of this post
+                echo "<a href='#' onclick='delete_user({$id});'  class='btn btn-danger'>Delete</a>";
+                echo "</td>";
+                echo "</tr>";
+            }
+
+
+            // end table
+            echo "</table>";
+        }
+        // if no records found
+        else {
+            echo "<div class='alert alert-danger'>No records found.</div>";
+        }
+        ?>
+
+
+
+    </div> <!-- end .container -->
+
+    <!-- confirm delete record will be here -->
+
+</body>
+
+</html>
