a998594cb02e466908f5efa3577e396ffbbb28eb
Lai Yong Xuan <yongxuan213012@dasein.edu.my>
2023-04-21 12:23:04 +0800

    change to if product2,3 is fill, quantity mustfill

--- order_create.php
+++ order_create.php
@@ -61,9 +61,21 @@ if (!isset($_SESSION['username'])) { // If the user is not logged in
                 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                 // Check if any field is empty
-                $customer_name_error = empty($customer_name) ? "Please select username" : "";
-                $product_error = empty($product) ? "Please select product" : "";
-                $quantity_error = empty($quantity) ? "Please enter product quantity" : "";
+                if (empty($customer_name)) {
+                    $customer_name_error = "Please enter username";
+                }
+                if (empty($product1)) {
+                    $product1_error = "Please select product";
+                }
+                if (empty($quantity1)) {
+                    $quantity1_error = "Please select product's quantity";
+                }
+                if (!empty($product2) && empty($quantity2)) {
+                    $quantity2_error = "Please select product's quantity";
+                }
+                if (!empty($product3) && empty($quantity3)) {
+                    $quantity3_error = "Please select product's quantity";
+                }
 
                 // Set default customer and product if not selected
                 $default_customer = $customers[0]['id'];
@@ -71,7 +83,10 @@ if (!isset($_SESSION['username'])) { // If the user is not logged in
 
                 // Bind customer and product to the query if selected, otherwise bind the default value
                 $stmt->bindParam(':customer_name', $customer_name);
-                $stmt->bindParam(':product', $product);
+                $stmt->bindParam(':product', $product1);
+                $stmt->bindParam(':product', $product2);
+                $stmt->bindParam(':product', $product3);
+
                 if (empty($customer_name)) {
                     $stmt->bindValue(':customer_name', $default_customer);
                 }
@@ -79,12 +94,14 @@ if (!isset($_SESSION['username'])) { // If the user is not logged in
                     $stmt->bindValue(':product', $default_product);
                 }
 
-                // Check if there are any errors
-                if (empty($customer_name_error) && empty($product_error) && empty($quantity_error)) {
+                // check if there are any errors
+                if (!isset($customer_name_error) && !isset($product1_error) && !isset($quantity1_error) && !isset($quantity2_error) && !isset($quantity3_error)) {
+
                     // Prepare and execute insert query for each product
                     for ($i = 0; $i < 3; $i++) {
                         $product = ${"product" . ($i + 1)};
                         $quantity = ${"quantity" . ($i + 1)};
+
                         if (!empty($product)) {
                             $query = "INSERT INTO orders SET customer_name=:customer_name, product=:product, quantity=:quantity, created=:created";
                             $stmt = $con->prepare($query);
@@ -95,6 +112,7 @@ if (!isset($_SESSION['username'])) { // If the user is not logged in
                             $created = date('Y-m-d H:i:s');
                             $stmt->bindParam(':created', $created);
 
+                            // Execute the query
                             if ($stmt->execute()) {
                                 echo "<div class='alert alert-success'>Record was saved.</div>";
                                 $customer_name = "";
@@ -116,12 +134,17 @@ if (!isset($_SESSION['username'])) { // If the user is not logged in
         }
         ?>
 
+
         <!-- html form here where the product information will be entered -->
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <table class='table table-hover table-responsive table-bordered'>
                 <tr>
                     <td>Username</td>
                     <td>
+                        <?php
+                        // Assign the value of customer_name to a variable
+                        $selected_customer = isset($_POST['customer_name']) ? $_POST['customer_name'] : '';
+                        ?>
                         <select name="customer_name" class="form-control">
                             <option value="">-- Select Username --</option>
                             <?php
@@ -133,12 +156,13 @@ if (!isset($_SESSION['username'])) { // If the user is not logged in
                             $stmt->execute();
                             $customer_name = $stmt->fetchAll(PDO::FETCH_ASSOC);
                             // dynamically populate the dropdown list with categories
-                            foreach ($customer_name as $customer_name) {
-                                $selected = isset($customer_name) && $customer_name == $customer_name['username'] ? 'selected' : '';
-                                echo "<option value='{$customer_name['username']}' {$selected}>{$customer_name['username']}</option>";
+                            foreach ($customer_name as $customer) {
+                                $selected = ($selected_customer == $customer['username']) ? 'selected' : '';
+                                echo "<option value='{$customer['username']}' {$selected}>{$customer['username']}</option>";
                             }
                             ?>
                         </select>
+
                         <?php if (isset($customer_name_error)) : ?>
                             <span class="text-danger"><?php echo $customer_name_error; ?></span>
                         <?php endif; ?>
@@ -166,15 +190,14 @@ if (!isset($_SESSION['username'])) { // If the user is not logged in
                             }
                             ?>
                         </select>
-                        <?php if (isset($product_error)) : ?>
-                            <span class="text-danger"><?php echo $product_error; ?></span>
+                        <?php if (isset($product1_error)) : ?>
+                            <span class="text-danger"><?php echo $product1_error; ?></span>
                         <?php endif; ?>
 
 
                     <td>Quantity</td>
                     <td><input type="number" name="quantity1" class="form-control" value="<?php echo isset($quantity1) ? htmlspecialchars($quantity1) : ''; ?>" />
-                        <?php if (isset($quantity_error)) { ?><span class="text-danger"><?php echo $quantity_error; ?></span><?php } ?></td>
-
+                        <?php if (isset($quantity1_error)) { ?><span class="text-danger"><?php echo $quantity1_error; ?></span><?php } ?></td>
                     </td>
                 </tr>
 
@@ -201,6 +224,7 @@ if (!isset($_SESSION['username'])) { // If the user is not logged in
 
                     <td>Quantity</td>
                     <td><input type="number" name="quantity2" class="form-control" value="<?php echo isset($quantity2) ? htmlspecialchars($quantity2) : ''; ?>" />
+                        <?php if (isset($quantity2_error)) { ?><span class="text-danger"><?php echo $quantity2_error; ?></span><?php } ?></td>
                     </td>
                 </tr>
 
@@ -227,7 +251,7 @@ if (!isset($_SESSION['username'])) { // If the user is not logged in
 
                     <td>Quantity</td>
                     <td><input type="number" name="quantity3" class="form-control" value="<?php echo isset($quantity3) ? htmlspecialchars($quantity3) : ''; ?>" />
-
+                        <?php if (isset($quantity3_error)) { ?><span class="text-danger"><?php echo $quantity3_error; ?></span><?php } ?></td>
                     </td>
                 </tr>
 
