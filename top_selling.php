581f2d974f27f0fceaa190d8ad60a38536106788
Lai Yong Xuan <yongxuan213012@dasein.edu.my>
2023-05-12 21:33:14 +0800

    change to top 5 selling product

--- top_selling.php
+++ top_selling.php
@@ -18,23 +18,24 @@
     // include database connection
     include 'config/database.php';
     try {
-        function get_top_selling_product($con)
+        function get_top_selling_products($con, $limit = 5)
         {
-            // fetch top selling product
+            // fetch top selling products
             $query = "SELECT products.id, products.name, products.price, SUM(order_details.quantity) AS total 
             FROM order_details 
             JOIN products ON order_details.product_id = products.id
             JOIN orders ON orders.id = order_details.order_id
             GROUP BY products.id, products.name, products.price
             ORDER BY total DESC 
-            LIMIT 1";
+            LIMIT :limit";
             $stmt = $con->prepare($query);
+            $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
             $stmt->execute();
-            return $stmt->fetch(PDO::FETCH_ASSOC);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
 
-        // get top selling product
-        $top_selling_product = get_top_selling_product($con);
+        // get top selling products (limit to 5)
+        $top_selling_products = get_top_selling_products($con, 5);
     } catch (PDOException $exception) {
         die('ERROR: ' . $exception->getMessage());
     }
@@ -43,23 +44,25 @@
     <div class="container" style="margin-top: 20px;">
         <div class="card" style="border-width: 3px; border-color:#496058; background-color: #496058;">
             <div class="card-header" style="color: white;">
-                <h2>Top Selling Product</h2>
+                <h2>Top Selling Products</h2>
             </div>
             <div class="card-body" style="background-color: white">
                 <div class="row">
-                    <?php if ($top_selling_product && is_array($top_selling_product)) : ?>
-                        <div class="col-md-4">
-                            <div class="card-body" style="color: black;">
-                                <h3 class="card-title" style="font-weight: bold; margin:0;"><?php echo ucfirst($top_selling_product['name']); ?></h3>
-                                <p class="card-text"><?php echo $top_selling_product['total']; ?> units sold</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <small class="text">RM<?php echo $top_selling_product['price']; ?></small>
-                                    <div class="btn-group">
-                                        <a href="product_read_one.php?id=<?php echo $top_selling_product['id']; ?>" class="btn btn-sm btn-outline-secondary" style="font-weight: bold; border-color:#496058;">View</a>
+                    <?php if ($top_selling_products && is_array($top_selling_products)) : ?>
+                        <?php foreach ($top_selling_products as $product) : ?>
+                            <div class="col-md-4">
+                                <div class="card-body" style="color: black;">
+                                    <h3 class="card-title" style="font-weight: bold; margin:0;"><?php echo ucfirst($product['name']); ?></h3>
+                                    <p class="card-text"><?php echo $product['total']; ?> units sold</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <small class="text">RM<?php echo $product['price']; ?></small>
+                                        <div class="btn-group">
+                                            <a href="product_read_one.php?id=<?php echo $product['id']; ?>" class="btn btn-sm btn-outline-secondary" style="font-weight: bold; border-color:#496058;">View</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        <?php endforeach; ?>
                     <?php endif; ?>
                 </div>
             </div>
